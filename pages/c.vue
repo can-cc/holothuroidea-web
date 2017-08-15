362596d009330e48f0e8c4f16323e89f7755c3b7
fwchen <crystal.tyan@gmail.com>
2017-08-15 16:04:33 +0800

    categorys and category

---
+++ pages/c.vue
@@ -0,0 +1,35 @@
+<template>
+  <section class="container">
+    <ul>
+      <li v-for="item in categorys">
+        <nuxt-link v-bind:to="item.name">{{item.name}}</nuxt-link>
+      </li>
+    </ul>
+    <transition name="fade">
+      <nuxt-child/>
+    </transition>
+  </section>
+</template>
+
+<script>
+  import axios from 'axios'
+
+  export default {
+    components: {
+
+    },
+    asyncData ({ params }) {
+      return axios.get(`/static/data/_summary.json`)
+        .then(res => {
+          return { categorys: res.data }
+        })
+    }
+  }
+</script>
+
+<style>
+  .container {
+    display: flex;
+    justify-content: flex-start;
+  }
+</style>
---
+++ pages/c/_category/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <section class="container">
+    <ul>
+      <li v-for="item in category.articles">
+        <div>{{item.title}}</div>
+        <div v-html="item.content"></div>
+      </li>
+    </ul>
+  </section>
+</template>
+
+<script>
+  import axios from 'axios'
+
+  export default {
+    components: {
+
+    },
+    asyncData ({ params }) {
+      return axios.get(`http://localhost:3000/data/${params.category}.json`)
+        .then(res => {
+          return { category: res.data }
+        })
+    }
+  }
+</script>
+
+<style>
+
+</style>
